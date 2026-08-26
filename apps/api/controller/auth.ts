16ae44b9c90dfa80488dc48316f43287448d3153
suhaibfida <suhaibfida33@outlook.com>
2026-08-26 16:17:18 +0530

    signup add

--- apps/api/controller/auth.ts
+++ apps/api/controller/auth.ts
@@ -1,9 +1,35 @@
 import {Request,Response} from "express"
+import {signupSchema} from "@repo/zod/zod"
 import {prisma} from "@repo/db/prisma"
-const signup =(req:Request,res:Response)=>{
-    const {username,email,password}=req.body
-   
-   
-
+interface user{
+    username:String,
+    email :String,
+    password:String
+}
+const signup =async (req:Request,res:Response)=>{
+    const {username,email,password}:user=req.body
+    const result=await signupSchema.safeParse({username,email,password});
+    if(!result.success){
+        return res.status(400).send({
+            message:"Please enter valid username,email,password"
+        })
+    }try{
+        console.log("here")
+    const user=await prisma.user.create({
+        data:{
+            username:result.data.username,
+            email:result.data.email,
+            password:result.data.password
+        }
+    })
+    console.log("here12")
+    
+        return res.status(200).send({
+        message:"Account created successfully"
+    })}
+    catch(error:any){
+        console.log(error)
+        
+    }
 }
 export default signup
---
+++ packages/zod/zod.ts
@@ -0,0 +1,9 @@
+import {z} from "zod";
+const signupSchema= z.object({
+    username:z.string(),
+    email:z.email(),
+    password:z.string().min(8).max(100)
+    })
+    
+
+export {signupSchema}
