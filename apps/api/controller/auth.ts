ec749d0fa00b18261b74c32d65f4be4ccc3ce2e2
suhaibfida <suhaibfida33@outlook.com>
2026-08-26 19:05:02 +0530

    signup v2

--- apps/api/controller/auth.ts
+++ apps/api/controller/auth.ts
@@ -7,13 +7,29 @@ interface user{
     password:String
 }
 const signup =async (req:Request,res:Response)=>{
-    const {username,email,password}:user=req.body
+    try{
+            const {username,email,password}:user=req.body
     const result=await signupSchema.safeParse({username,email,password});
-    if(!result.success){
-        return res.status(400).send({
+     if(!result.success){
+        return res.status(400).json({
             message:"Please enter valid username,email,password"
         })
-    }try{
+    }
+    const findCheck=await prisma.user.findFirst({
+        where:{
+            OR:[
+                {username:result.data.username},
+                {email:result.data.email}
+            ]
+        }
+    })
+    if(findCheck)
+        {
+            return res.status(400).json({
+
+                message:"Email or Username already exists"
+            })
+        }
         console.log("here")
     const user=await prisma.user.create({
         data:{
@@ -23,13 +39,11 @@ const signup =async (req:Request,res:Response)=>{
         }
     })
     console.log("here12")
-    
         return res.status(200).send({
         message:"Account created successfully"
     })}
     catch(error:any){
         console.log(error)
-        
     }
 }
 export default signup
