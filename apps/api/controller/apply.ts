8669f0fa6245941899d6ee2576562bd2a59b3add
suhaibfida <suhaibfida33@outlook.com>
2026-09-20 21:26:27 +0530

    add apply.ts

--- apps/api/controller/apply.ts
+++ apps/api/controller/apply.ts
@@ -2,12 +2,13 @@ import {Request,Response} from "express"
 import {prisma} from "@repo/db/prisma"
 export const apply=(req:Request,res:Response)=>{
     const {appliedTo,link,description}=req.body
+    const userId=req.id
     const apply=prisma.user.create({
         data:{
             appliedTo,
             link,
             description,
-            req.id
+            userId
         }
     })
        res.status(200).json({
